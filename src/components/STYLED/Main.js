8ae2eb73c73b894bb420c573f9f815fa302fae0b
VVKo <v.v.kovdrysh@gmail.com>
2023-03-11 13:28:37 +0200

    pre public
    Signed-off-by: VVKo <v.v.kovdrysh@gmail.com>

--- src/components/STYLED/Main.js
+++
@@ -1,9 +0,0 @@
-import tw from "tailwind-styled-components";
-
-export const MainTW = tw.div`
-${(p) => (p.$activeMenu ?
-        "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
-        :
-        "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
-)}
-`;
--- src/components/STYLED/index.js
+++
@@ -1,7 +0,0 @@
-export {AppStyles} from "./App";
-export {SettingsStyles as Settings} from "./App";
-export {TooltipBtnStyles as TooltipBtn} from './App'
-export {SidebarStyles as SidebarWrap} from './Sidebar'
-export {SidebarHeaderStyles as SidebarHeader} from './Sidebar'
-export {SidebarContainerStyles as SidebarContainer} from './Sidebar'
-export {SidebarBodyStyles as SidebarBody} from './Sidebar'
